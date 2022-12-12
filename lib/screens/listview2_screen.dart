321dd60e9618e3ce336486ccbe1aa41e126ad865
Bryan Parada <bryan.parada.cea@gmail.com>
2022-12-12 20:27:13 -0300

    F67 - Taps about ListTiles - style AppBar

--- lib/screens/listview2_screen.dart
+++ lib/screens/listview2_screen.dart
@@ -13,15 +13,21 @@ class Listview2Screen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Listview Type 2'),
+        elevation: 5,
+        backgroundColor: Colors.indigo,
       ),
       body: ListView.separated(
         itemCount: options.length,
         itemBuilder: (context, i) => ListTile( 
                     title: Text(options[i]),
-                    trailing: const Icon( Icons.arrow_forward_ios_outlined)), //Text(options[index])
-        separatorBuilder: ( _, __ ) => const Divider())
-
-  
+                    trailing: const Icon( Icons.arrow_forward_ios_outlined, color: Colors.indigo), //Text(options[index])
+                    onTap: () {
+                      final game = options[i];
+                      print(game);
+                    },
+      ),
+        separatorBuilder: ( _, __ ) => const Divider(),
+        ) 
     );
   }
 }
