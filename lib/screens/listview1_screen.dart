0c6c2b0aa309abca1ecd1e60cbcddd0f90e4a906
Bryan Parada <bryan.parada.cea@gmail.com>
2022-12-12 18:02:14 -0300

    f64 - listView

---
+++ lib/screens/listview1_screen.dart
@@ -0,0 +1,26 @@
+
+
+import 'package:flutter/material.dart';
+
+class Listview1Screen extends StatelessWidget {
+   
+  const Listview1Screen({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Listview Type 1'),
+      ),
+      body: ListView(
+        children: const [
+          Text('hi'),
+          Text('hi'),
+          Text('hi'),
+          Text('hi'),
+          Text('hi'),
+        ],
+      ),
+    );
+  }
+}
