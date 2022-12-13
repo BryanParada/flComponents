5301e5672414f3f384ed0b371ceec479b60b195c
Bryan Parada <bryan.parada.cea@gmail.com>
2022-12-13 18:26:05 -0300

    F82 - Show Alert

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -3,6 +3,40 @@ import 'package:flutter/material.dart';
 class AlertScreen extends StatelessWidget {
    
   const AlertScreen({Key? key}) : super(key: key);
+
+  void displayDialog(BuildContext context){
+   
+   showDialog(
+    barrierDismissible: false,
+      context: context, 
+      builder: (context ) {
+
+        return  AlertDialog(
+          elevation: 5,
+          title: const Text('Title'),
+          shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(10)),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [ 
+              Text('This is the content of the alert'),
+              SizedBox(height: 10),
+              FlutterLogo( size: 100)
+          ]),
+
+          actions: [
+            TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text('Cancel')
+              )
+
+          ]
+        );
+        
+      }
+      );
+
+
+  }
   
   @override
   Widget build(BuildContext context) {
@@ -18,9 +52,8 @@ class AlertScreen extends StatelessWidget {
             padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
             child: Text('Show Alert', style: TextStyle( fontSize: 25)),
           ), 
-          onPressed:() {
-            
-          },
+          onPressed: () => displayDialog(context), //() cuando usamos Buildcontext
+
          ),
       ),
       floatingActionButton: FloatingActionButton(
