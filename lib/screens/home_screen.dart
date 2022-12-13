c7fa900aaca35ef5ba5d9b541dac52c48680ef12
Bryan Parada <bryan.parada.cea@gmail.com>
2022-12-13 10:29:11 -0300

    F69 - HomeScreen design

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,10 +6,21 @@ class HomeScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('HomeScreen'),
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Components'),
+        elevation: 0, //sombra
       ),
+      body: ListView.separated(
+        itemBuilder: (context, index) => ListTile(
+          leading: const Icon( Icons.access_time_outlined),
+          title: const Text('Route name'),
+          onTap: (){
+            
+          },
+        ),
+        separatorBuilder: (_,__) => const Divider(),
+        itemCount: 10)
     );
   }
 }
