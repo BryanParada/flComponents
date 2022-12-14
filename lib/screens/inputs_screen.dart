1f6d7b6f139a32aa1dc976165c48ec408753880a
Bryan Parada <bryan.parada.cea@gmail.com>
2022-12-14 17:24:24 -0300

    F88 - TextFormFields - Customize

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -17,11 +17,36 @@ class InputsScreen extends StatelessWidget {
 
               TextFormField(
                 autofocus: true,
-                initialValue: 'Bryan Parada',
+                initialValue: '',
                 textCapitalization: TextCapitalization.words,
                 onChanged: (value) {
                   print('value: $value');
                 },
+                validator:(value) {
+                  if ( value == null ) return 'This field is required';
+                  return value.length <3 ? '3 Letters minimun' : null;
+                },
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: const InputDecoration(
+                  hintText: 'User Name placeholder',
+                  labelText: 'Name',
+                  helperText: 'Only letters',
+                  counterText: '3 characters',
+                  //prefixIcon: Icon( Icons.verified_user_outlined),
+                  suffixIcon: Icon( Icons.group),
+                  icon: Icon( Icons.assignment_ind_outlined),
+                  focusedBorder: OutlineInputBorder( //focusedBorder quitara los estilos de border: OutlineInputBorder v
+                    borderSide: BorderSide(
+                      color: Colors.green
+                    )
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.only(
+                       bottomLeft: Radius.circular(10),
+                       topRight: Radius.circular(10)
+                       ) 
+                     )
+                  )
               )
 
           ],)
